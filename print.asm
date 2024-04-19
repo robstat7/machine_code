a082ae5c907eec582cbae2127ca8c27fd48b313c
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2024-04-19 18:24:54 +0530

    initial commit

---
+++ print.asm
@@ -0,0 +1,17 @@
+format ELF64 executable 3	; value 3 marks the executable for Linux system
+
+;print
+; code this part begins
+mov eax, 1	; syscall #1 (write)
+mov esi, input	; arg1 = msg
+mov edx, 6	; arg2 = msg length
+mov edi, 1	; arg3 = 1 (stdout)
+syscall		; call write
+; code this part ends
+
+; exit
+mov rax, 60     ; syscall #60 (exit)
+mov rdi, 0      ; arg 1 = 0 (OK)
+syscall         ; call exit
+
+input db "hello", 10, 0
