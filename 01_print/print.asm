7403e40aaa0cc6b7924091400b00241f1e8d7d3c
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2024-04-20 08:54:39 +0530

    update info

--- 01_print/print.asm
+++ 01_print/print.asm
@@ -1,13 +1,13 @@
 format ELF64 executable 3	; value 3 marks the executable for Linux system
 
-;print
 ; code this part begins
+
+; print
 mov eax, 1	; syscall #1 (write)
 mov esi, input	; arg1 = msg
 mov edx, 6	; arg2 = msg length
 mov edi, 1	; arg3 = 1 (stdout)
 syscall		; call write
-; code this part ends
 
 ; exit
 mov eax, 60     ; syscall #60 (exit)
@@ -15,3 +15,5 @@ mov edi, 0      ; arg 1 = 0 (OK)
 syscall         ; call exit
 
 input db "hello", 10, 0
+
+; code this part ends
